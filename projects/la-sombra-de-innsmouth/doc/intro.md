3c48e41a586117807ab5dc0905a1efbc3752d525
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-10-24 21:26:48 -0300

    Añadido: proyecto la sombra de innsmouth

---
+++ projects/la-sombra-de-innsmouth/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to la-sombra-de-innsmouth
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ projects/la-sombra-de-innsmouth/test/la_sombra_de_innsmouth/core_test.clj
@@ -0,0 +1,7 @@
+(ns la-sombra-de-innsmouth.core-test
+  (:require [clojure.test :refer :all]
+            [la-sombra-de-innsmouth.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
